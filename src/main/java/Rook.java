d7e996bd356f2f1ee5bc321d033b9844ea655461
jesslenn <jessica.lennick@gmail.com>
2024-11-30 15:17:44 -0500

    mid-afternoon break point; started Rook

--- src/main/java/Rook.java
+++ src/main/java/Rook.java
@@ -1,2 +1,18 @@
-public class Rook {
+public class Rook extends Piece {
+    public Rook(PieceColor color, Position position) {
+        super(color, position);
+    }
+
+    @Override
+    public boolean isValidMove(Position newPosition, Piece[][] board) {
+        //we need to know what direction to move in and the difference in positions
+        int forwardDirection = color == PieceColor.WHITE ? -1 : 1;
+        int rowDiff = (newPosition.getRow() - position.getRow()) * forwardDirection;
+        int colDiff = (newPosition.getColumn() - position.getColumn());
+
+        //if moving straight down the row
+        if (position.getRow() == newPosition.getRow()) {
+
+        }
+    }
 }
