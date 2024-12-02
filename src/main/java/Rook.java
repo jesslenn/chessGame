48070dd1a88e7c651f1313b38d9b98b61ba1755a
jesslenn <jessica.lennick@gmail.com>
2024-12-02 10:41:01 -0500

    finished Rook class behavior

--- src/main/java/Rook.java
+++ src/main/java/Rook.java
@@ -12,7 +12,31 @@ public class Rook extends Piece {
 
         //if moving straight down the row
         if (position.getRow() == newPosition.getRow()) {
-
+            //starting at the next available square for this movement
+            int columnStart = Math.min(position.getColumn(), newPosition.getColumn()) + 1;
+            int columnEnd = Math.max(position.getColumn(), newPosition.getColumn());
+            for (int column = columnStart; column < columnEnd; column++) {
+                if (board[position.getRow()][column] != null) {
+                    return false;
+                }
+            }
+        } else if (position.getColumn() == newPosition.getColumn()) {
+            int rowStart = Math.min(position.getRow(), newPosition.getRow()) + 1;
+            int rowEnd = Math.max(position.getRow(), newPosition.getRow());
+            for (int row = rowStart; row < rowEnd; row++) {
+                if (board[row][position.getColumn()] != null) {
+                    return false;
+                }
+            }
+        } else {
+            return false;
         }
+        Piece destinationPiece = board[newPosition.getRow()][newPosition.getColumn()];
+        if (destinationPiece == null) {
+            return true;
+        } else if (destinationPiece.getColor() != this.getColor()) {
+            return true;
+        }
+        return false;
     }
 }
