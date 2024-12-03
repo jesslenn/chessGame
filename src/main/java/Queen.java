522abc0791abb232c751f0429aefb6149cb56b19
jesslenn <jessica.lennick@gmail.com>
2024-12-03 17:53:42 -0500

    Finished Queen class logic

--- src/main/java/Queen.java
+++ src/main/java/Queen.java
@@ -8,5 +8,32 @@ public class Queen extends Piece {
         if (newPosition.equals(this.position)) {
             return false;
         }
+
+        int rowDiff = Math.abs(newPosition.getRow() - position.getRow());
+        int colDiff = Math.abs(newPosition.getColumn() - position.getColumn());
+
+        boolean straightLine = this.position.getRow() == newPosition.getRow() || this.position.getColumn() == newPosition.getColumn();
+        boolean diagonal = rowDiff == colDiff;
+
+        if (!straightLine && !diagonal) {
+            return false;
+        }
+
+        int rowDirection = Integer.compare(newPosition.getRow(), this.position.getRow());
+        int colDirection = Integer.compare(newPosition.getColumn(), this.position.getColumn());
+
+        int currentRow = this.position.getRow() + rowDirection;
+        int currentCol = this.position.getColumn() + colDirection;
+
+        while(currentRow != newPosition.getRow() || currentCol != newPosition.getColumn()) {
+            if (board[currentRow][currentCol] != null) {
+                return false;
+            }
+            currentRow += rowDirection;
+            currentCol += colDirection;
+        }
+
+        Piece destinationPiece = board[newPosition.getRow()][newPosition.getColumn()];
+        return destinationPiece == null || destinationPiece.getColor() != this.getColor();
     }
 }
