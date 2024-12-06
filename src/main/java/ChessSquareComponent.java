a031d93e1f3123d6e5f27c4e5d4f4d98dc9a13eb
jesslenn <jessica.lennick@gmail.com>
2024-12-06 17:53:17 -0500

    added methods to chess square component needed for GUI

--- src/main/java/ChessSquareComponent.java
+++ src/main/java/ChessSquareComponent.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import javax.swing.JButton;
 
 public class ChessSquareComponent extends JButton {
@@ -7,6 +8,14 @@ public class ChessSquareComponent extends JButton {
     public ChessSquareComponent(int row, int col) {
         this.row = row;
         this.col = col;
+    }
 
+    public void setPieceSymbol(String symbol, Color color) {
+        this.setText(symbol);
+        this.setForeground(color);
+    }
+
+    public void clearPieceSymbol() {
+        this.setText("");
     }
 }
